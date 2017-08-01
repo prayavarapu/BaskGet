6f0b373d81c0c90fa6656d59c7ce079367d31237
Pratyusha Rayavarapu <pratyusharayavarapu@PR-MacBook-Air.fios-router.home>
2017-08-01 19:47:24 -0400

    authentication established and working

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@ var express = require('express');
 var router = express.Router();
 
 // Get Homepage
-router.get('/', function(req, res){
+router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
 });
+
+function ensureAuthenticated(req, res){
+
+	if(req.isAuthenticated()){
+		return next();
+
+	}
+	else{
+		res.redirect("/users/login");
+	}
+}
 
 module.exports = router;
