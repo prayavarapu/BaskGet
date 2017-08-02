c1fcf4cbe8d76be51e677ec9d4ea510587c3ade0
Pratyusha Rayavarapu <pratyusharayavarapu@PR-MacBook-Air.fios-router.home>
2017-08-01 21:01:47 -0400

    authentication working

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,12 @@ router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
 });
 
-function ensureAuthenticated(req, res){
-
+function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
-
-	}
-	else{
-		res.redirect("/users/login");
+	} else {
+		//req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
 	}
 }
 
